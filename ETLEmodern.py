01bfb2d85e5bb463606e63103b78a97eb0f5c0f5
Soldaten <2705nathaniel@gmail.com>
2026-05-20 16:11:30 +0700

    Memindah centroid ke bottom bounding box

--- ETLEmodern.py
+++ ETLEmodern.py
@@ -18,10 +18,13 @@ print("FPS:", fps)
 
 LINE_Y1 = 400
 LINE_Y2 = 500
+LINE_Y3 = 500
+LINE_Y4 = 400
 
 real_world_distance = 10  # meters
 
-vehicle_times = {}
+vehicle_times_in = {}
+vehicle_times_out = {}  
 vehicle_speeds = {}
 previous_positions = {}
 
@@ -33,21 +36,22 @@ while True:
     if not ret:
         break
 
-    results = model.track(frame, persist=True, tracker="bytetrack.yaml", classes=[2, 3, 5, 7], verbose=False)  # Filter for vehicles (car, motorcycle, bus, truck)
+    results = model.track(frame, persist=True, tracker="bytetrack.yaml", conf=0.4, iou=0.5, classes=[2, 3, 5, 7], verbose=False)  # Filter for vehicles (car, motorcycle, bus, truck)
 
     cv2.line(frame, (0, LINE_Y1), (frame.shape[1], LINE_Y1), (0, 255, 0), 2)
     cv2.line(frame, (0, LINE_Y2), (frame.shape[1], LINE_Y2), (0, 255, 0), 2)
-
-    
+    cv2.line(frame, (0, LINE_Y3), (frame.shape[1], LINE_Y3), (0, 255, 0), 2)
+    cv2.line(frame, (0, LINE_Y4), (frame.shape[1], LINE_Y4), (0, 255, 0), 2)
 
     if results[0].boxes.id is not None:
         boxes = results[0].boxes.xyxy.cpu().numpy()
         ids = results[0].boxes.id.cpu().numpy()
 
         for box, track_id in zip(boxes, ids):
+            track_id = int(track_id)
             x1, y1, x2, y2 = map(int, box)
             cx = int((x1 + x2) / 2)
-            cy = int((y1 + y2) / 2)
+            cy = y2
             cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
             cv2.circle(frame, (cx, cy), 5, (255, 0, 0), -1)
 
@@ -56,12 +60,27 @@ while True:
 #Kendaraan lewat garis pertama
                 if prev_cy < LINE_Y1 and cy >= LINE_Y1:
 
-                    if track_id not in vehicle_times:
-                        vehicle_times[track_id] = frame_count
+                    if track_id not in vehicle_times_in:
+                        vehicle_times_in[track_id] = frame_count
          #Kendaraan Lewat garis kedua       
                 if prev_cy < LINE_Y2 and cy >= LINE_Y2:
-                    if track_id in vehicle_times and track_id not in vehicle_speeds:
-                        frame_difference = frame_count - vehicle_times[track_id]
+                    if track_id in vehicle_times_in and track_id not in vehicle_speeds:
+                        frame_difference = frame_count - vehicle_times_in[track_id]
+                        time_taken = frame_difference / fps
+
+                        if time_taken > 0:
+                            speed_mps = real_world_distance / time_taken
+                            speed_kmph = speed_mps * 3.6
+
+                            vehicle_speeds[track_id] = speed_kmph
+           #Outgoing Vehicle                 
+                if prev_cy > LINE_Y3 and cy <= LINE_Y3:
+                    if track_id not in vehicle_times_out:
+                        vehicle_times_out[track_id] = frame_count
+
+                if prev_cy > LINE_Y4 and cy <= LINE_Y4:
+                    if track_id in vehicle_times_out and track_id not in vehicle_speeds:
+                        frame_difference = frame_count - vehicle_times_out[track_id]
                         time_taken = frame_difference / fps
 
                         if time_taken > 0:
@@ -72,6 +91,8 @@ while True:
 
             previous_positions[track_id] = cy
 
+            
+
 
             if track_id in vehicle_speeds:
                 speed_text = f'ID: {int(track_id)} Speed: {vehicle_speeds[track_id]:.2f} km/h'
@@ -80,10 +101,10 @@ while True:
             
             cv2.putText(frame, speed_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2) 
 
-            cv2.putText(frame, "LINE 1", (20, LINE_Y1 - 10),
+            cv2.putText(frame, "LINE", (20, LINE_Y1 - 10),
                         cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.7, (0,255,0), 2)     
 
-            cv2.putText(frame, "LINE 2", (20, LINE_Y2 - 10),
+            cv2.putText(frame, "LINE", (20, LINE_Y2 - 10),
                         cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.7, (0,255,0), 2)
 
             print(f"ID : {track_id}, CY: {cy}")   
